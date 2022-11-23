e989e0db453356ed4997e112bc8c9ef755161166
devWaz <fawazodessy@gmail.com>
2022-11-23 23:46:19 +0100

    Setup

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Navbar = () => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+
+export default Navbar;
